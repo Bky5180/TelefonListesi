4a45e6a12318944743aa8a7b1f1b094a5d05427a
busranurbaydur <busranurbaydur@outlook.com>
2021-04-13 00:52:55 +0300

    Add project files.

---
+++ TelefonListesi/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelefonListesi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Telefon telefon1 = new Telefon();
+
+            telefon1.TelefonId = 1;
+            telefon1.Marka = "Samsung Galaksi S10";
+            telefon1.Fiyat = 5499;
+            telefon1.Renk = Color.White;
+            telefon1.EkranBoyutu = 6.4;
+            telefon1.Agirlik = 175;
+            telefon1.KameraCözünürlügü = 12;
+            telefon1.DahiliDepolama = 128;
+            telefon1.RAM = 6;
+            telefon1.BataryaKapasitesi = 3400;
+            telefon1.IsletimSistemi = IsletimSistemi.Android;
+            telefon1.SuyaDayaniklilik = true;
+            telefon1.ParmakIziOkuyucu = true;
+            telefon1.CPUFrekansi = 2.73;
+
+            Telefon telefon2 = new Telefon();
+
+            telefon2.TelefonId = 2;
+            telefon2.Marka = "Iphone 12 Pro";
+            telefon2.Fiyat = 13795;
+            telefon2.Renk = Color.Gold;
+            telefon2.EkranBoyutu = 6.1;
+            telefon2.Agirlik = 189;
+            telefon2.KameraCözünürlügü = 12;
+            telefon2.DahiliDepolama = 128;
+            telefon2.RAM = 6;
+            telefon2.BataryaKapasitesi = 2815;
+            telefon2.IsletimSistemi = IsletimSistemi.İOS;
+            telefon2.SuyaDayaniklilik = true;
+            telefon2.ParmakIziOkuyucu = true;
+            telefon2.CPUFrekansi = 3.0;
+
+            Telefon telefon3 = new Telefon();
+
+            telefon3.TelefonId = 3;
+            telefon3.Marka = "Huawei P20 Lite";
+            telefon3.Fiyat = 1949;
+            telefon3.EkranBoyutu = 5.84;
+            telefon3.Agirlik = 145;
+            telefon3.KameraCözünürlügü = 16;
+            telefon3.DahiliDepolama = 64;
+            telefon3.BataryaKapasitesi = 3000;
+            telefon3.IsletimSistemi = IsletimSistemi.Android;
+            telefon3.CPUFrekansi = 2.36;
+
+            Console.WriteLine("------------------------------------------BUSRA TELEFON DÜNYASI---------------------------------------------------");
+            Console.WriteLine("                                                                                 ");
+            Telefon[] telefonlar = new Telefon[] { telefon1, telefon2, telefon3 };
+
+            foreach (var telefon in telefonlar)
+            {
+                Console.WriteLine
+                    ("Telefon Markası: " + telefon.Marka + " / Telefon Fiyatı: " + telefon.Fiyat + " TL" + "\n" +
+                    "Telefon Özellikleri " +
+                    "\n" + "Renk :" + telefon.Renk + " / Ekran Boyutu : " + telefon.EkranBoyutu + " inç " + " / Ağırlık : " + telefon.Agirlik + " Gr." +
+                    "\n" + "Kamera Çözünürlüğü : " + telefon.KameraCözünürlügü + " MP " + " / Dahili Depolama : " + telefon.DahiliDepolama + " GB " + " / RAM : " + telefon.RAM + " GB " + " / Batarya Kapasitesi : " + telefon.BataryaKapasitesi + " mAh " +
+                    "\n" + "İşletim Sistemi : " + telefon.IsletimSistemi + " / Suya Dayanıklılık : " + telefon.SuyaDayaniklilik + " / Parmak İzi Okuma : " + telefon.ParmakIziOkuyucu + " / CPU Frekansı : " + telefon.CPUFrekansi + " GHz"
+                    );
+                Console.WriteLine("---------------------------------------------------------------------------------------------------------------");
+
+
+            }
+
+            Console.ReadLine();
+
+            //Console.WriteLine(telefon1.Agirlik);
+        }
+    }
+}
---
+++ TelefonListesi/Telefon.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelefonListesi
+{
+    class Telefon
+    {
+        public Telefon()
+        {
+            Fiyat = 7000;
+            Renk = Color.Black;
+            Agirlik = 150;
+            RAM = 8;
+            CPUFrekansi = 2.73;
+            SuyaDayaniklilik = false;
+            ParmakIziOkuyucu = false;
+        }
+
+        public int TelefonId { get; set; }
+        public string Marka { get; set; }
+        public decimal Fiyat { get; set; }
+        public Color Renk { get; set; }
+        public double EkranBoyutu { get; set; }
+        public int Agirlik { get; set; }
+        public int KameraCözünürlügü { get; set; }
+        public int DahiliDepolama { get; set; }
+        public int RAM { get; set; }
+        public int BataryaKapasitesi { get; set; }
+        public IsletimSistemi IsletimSistemi { get; set; }
+        public bool SuyaDayaniklilik { get; set; }
+        public bool ParmakIziOkuyucu { get; set; }
+        public double CPUFrekansi { get; set; }
+
+
+
+    }
+    public enum IsletimSistemi
+    {
+        Android,
+        İOS,
+        BlackBerry
+
+    }
+
+    //public class Agirlik
+    //{
+    //    public int En { get; set; }
+    //    public int Boy { get; set; }
+    //}
+
+
+}
+
